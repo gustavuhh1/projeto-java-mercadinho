f058efede53da36adfe72876ff7b3ecb0c84c9de
GustavoMartinsR <gustavomrodrigues11@gmail.com>
2024-12-06 15:29:49 -0300

    add github

--- src/main/java/Identificavel.java
+++
@@ -1,3 +0,0 @@
-public interface Identificavel {
-    int getId();
-}
--- src/main/java/Persistencia.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-import java.util.List;
-
-public interface Persistencia {
-    void salvarArquivo(String caminho, List<Produto> produtos) throws IOException;
-    List<Produto> carregarArquivo(String caminho) throws IOException;
-}
